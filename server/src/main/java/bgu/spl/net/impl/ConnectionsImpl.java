715d33322a12c39c91f78b7ccd1559c344fcbc71
אומר אליהו <omereliy@post.bgu.ac.il>
2023-01-08 17:12:12 +0200

    implemtation of connectionIMPL Stompmessaging

---
+++ server/src/main/java/bgu/spl/net/impl/ConnectionsImpl.java
@@ -0,0 +1,93 @@
+package bgu.spl.net.impl;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+//import java.util.Dictionary;
+import java.util.WeakHashMap;
+
+import bgu.spl.net.srv.ConnectionHandler;
+import bgu.spl.net.srv.Connections;
+
+public class ConnectionsImpl<T> implements Connections<T> {
+    WeakHashMap<Integer, ConnectionHandler<T>> connections = new WeakHashMap<Integer,ConnectionHandler<T>>();;//clientId, connectionHandler
+    List <ConnectionHandler<T>> connectionHandlers= new LinkedList<ConnectionHandler<T>>();;
+    HashMap<Integer,HashMap<Integer,String>> topicsOfClient=new HashMap<>();// [client id, and [topic id, sbscribed strings]]    [integer,[integer,string]]
+    HashMap<String, List<Integer[]>> subscriptionsOfChannel= new HashMap();// topic name and another hash map with client id and its topic id// 
+    HashMap<String,String> UserNameToPassword= new HashMap<>();//[Name,Password]
+    HashMap<Integer,String> UserNameToConnctionId =new HashMap<>();//[Name,ConnectionId]
+    @Override
+    public boolean send(int connectionId, T msg) {
+        try{
+            connections.get(connectionId).send(msg);
+            return true;
+        }
+  
+            catch(Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public void send(String channel, T msg, int connectionId){
+         
+        List<Integer[]> ClientIDtoTopicID= subscriptionsOfChannel.get(channel);
+        for(Integer[] arr: ClientIDtoTopicID)
+        {
+            send(arr[0], msg);
+        }
+    }
+
+    @Override
+    public void disconnect(int connectionId) {
+        connections.remove(connectionId);
+        topicsOfClient.remove(connectionId);   
+        for(String topicId: subscriptionsOfChannel.keySet()){
+            for(Integer[] arr :subscriptionsOfChannel.get(topicId)){
+                if(arr[1]==connectionId)
+                    subscriptionsOfChannel.get(topicId).remove(arr);//not sure if it works
+            }
+        }
+    }
+
+    public void connect(String name,String password,int conntionId){//not sure if i need a new connection handler or should i give him one
+        UserNameToPassword.put(name, password);
+        UserNameToConnctionId.put(conntionId, name);
+        //how to add a connection handler ???SSS
+        
+    }
+
+    public boolean IsSubscribed(int connectionId, String chanel)
+    {
+        HashMap<Integer,String> map =topicsOfClient.get(connectionId);
+        Collection<String>c= map.values();
+        if(!c.contains(chanel))
+            return false;
+
+            return true;
+    }
+
+    public void subscribe(int connectionId, String chanel, int topicId){
+    
+        boolean connected=false;
+        for(Integer[] a: subscriptionsOfChannel.get(chanel))
+        {
+            if(a[0]==connectionId)
+                connected=true;
+        }
+        
+        if(!connected){
+            HashMap<Integer,String> map =topicsOfClient.get(connectionId);
+        map.put(topicId, chanel);
+        Integer[] arr= {connectionId,topicId};
+         subscriptionsOfChannel.get(chanel).add(arr);
+        }
+    }
+    public void addHandler(ConnectionHandler<T> handler,int connectionId){
+        connectionHandlers.add(handler);
+        connections.put(connectionId, handler);
+    }
+    
+}
+
--- server/src/main/java/bgu/spl/net/srv/Connections.java
+++ server/src/main/java/bgu/spl/net/srv/Connections.java
@@ -2,11 +2,15 @@ package bgu.spl.net.srv;
 
 import java.io.IOException;
 
+
 public interface Connections<T> {
 
     boolean send(int connectionId, T msg);
 
-    void send(String channel, T msg);
+    void send(String channel, T msg,int connectionId);
 
     void disconnect(int connectionId);
+
+    void connect(String username, String password, int connectionId);
+    public void addHandler(ConnectionHandler<T> handler,int connectionId);
 }
