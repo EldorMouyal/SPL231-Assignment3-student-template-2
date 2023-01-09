e2c7accd32d88840e5e3342e5364763a57f68087
EldorMouyal <eldor121@gmail.com>
2023-01-10 01:01:37 +0200

    stompFrame partly implemented

--- client/include/stompFrame.h
+++ client/include/stompFrame.h
@@ -6,16 +6,26 @@
 class stompFrame{
 
 private :
-std::string header;
-std::vector<std::string> ops;
+std::string command;
+std::vector<std::string> headers;
 std::string body;
-
-public:
-stompFrame();
+std::string frame;
+int clientID;
+int receipt;
+void buildFrame();
 void addCommand(std::string header);
 void addHeader(std::string key, std::string value);
 void addBody(std::string);
-bool isFrameComplete();
+void createLoginFrame(std::string host, std::string port, std::string username, std::string password);
+void createJoinFrame(std::string gameName);
+void createExitFrame(std:: string gameName, int receipt);
+void createReportFrame(string report);
+void createLogoutFrame(int receipt);
+
+
+public:
+stompFrame(string line, int id, int receipt);
 std::string getFrame();
+void addBody(std::string);
 
 };
--- client/src/stompFrame.cpp
+++ client/src/stompFrame.cpp
@@ -1,33 +1,89 @@
 #include "../include/stompFrame.h"
 using namespace std;
 
-stompFrame:: stompFrame(){
-    
+stompFrame:: stompFrame(string line, int clientid, int _receipt){
+this->clientID = clientid;
+this->receipt = _receipt;
+
+int i=0 ;
+string command = "";
+while(i<line.length()){
+command += line[i];
+if(command == "login"){
+    addCommand("CONNECT");//createLoginFrame
+    break;
+}
+if(command == "join"){
+    addCommand("SUBSCRIBE");
+    break;
+}
+if(command == "exit"){
+    addCommand("UNSUBSCRIBE");
+    break;
+}
+if(command == "report"){
+    addCommand("SEND");
+    break;
+}
+if(command == "logout"){
+    addCommand("DISCONNECT");
+}
+}
 }
 
-void stompFrame:: addHeader(string _header){
-    if(_header.length() >0)
-        this->header = _header;
+void stompFrame:: createLoginFrame(string host, string port, string username, string password){
+    addCommand("CONNECT");
+    addHeader("accept-version","1.2");
+    addHeader("host","stomp.cs.bgu.ac.il");
+    addHeader(username,password);
+}
+
+void stompFrame:: createJoinFrame(string gameName){
+    addCommand("SUBSCRIBE");
+    addHeader("destination","/"+gameName);
+    addHeader("id", ""+ clientID);
+    addHeader("receipt", ""+ receipt);
+}
+
+void stompFrame:: createExitFrame(string gameName, int receipt){// where do i get the receipt id according to the gameName?
+    addCommand("UNSUBSCRIBE");
+    addHeader("id", ""+ clientID);
+    addHeader("receipt", ""+ receipt);
+}
+void stompFrame:: createReportFrame(string report){
+    addCommand("SEND");// TODO: IMPLEMENT LATER
+}
+ void stompFrame:: createLogoutFrame(int receipt){
+    addCommand("DISCONNECT");
+    addHeader("receipt", ""+receipt);
+ }
+
+void stompFrame:: addCommand(string _command){  
+        this->command = _command;
 }
 
 void stompFrame:: addBody(string _body){
-    if(_body.length() >0)
+    if(_body.length() > 0)
         this->body += _body;
 }
 
-void stompFrame:: addOp(string op){
-    if(op.length() > 0)
-        ops.push_back(op);
+void stompFrame:: addHeader(string key, string value){
+     string header =  key + ":" + value + "\n";
+     headers.push_back(header);
 }
 
-string stompFrame:: getFrame(){
-    string out = header + "\n";
-    for(int i=0; i<ops.size(); i++){
-        out += (ops[i]);
+void stompFrame:: buildFrame(){
+    string out = command + "\n";
+    for(int i=0; i<headers.size(); i++){
+        out += (headers[i]);
         out += "\n";
     }
-    out += "\n";
 
-    out += (body);
+    out += "\n";// 
+
+    if(body.length() >0)
+        out += (body) + "\n";
+
+    out += "\0";
 }
 
