f1d43a2903642b78a8db21395e53affbd1294069
EldorMouyal <eldor121@gmail.com>
2023-01-12 15:07:19 +0200

    illegal patch to StompServer.main

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -7,6 +7,7 @@ import bgu.spl.net.srv.Server;
 public class StompServer {
 
     public static void main(String[] args) {
+        args = new String[] {"127.0.0.1", "tpc"};
         if(args[1].equals("reactor"))
             Server.reactor(
                     Runtime.getRuntime().availableProcessors(),
