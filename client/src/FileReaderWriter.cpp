01cde0a31aa6e3d13707ff92c4580d4de3012276
omereliy <omereliy@post.bgu.ac.il>
2023-01-13 12:49:07 +0200

    ReaderWriter not tested but no errors

--- client/src/FileReaderWriter.cpp
+++ client/src/FileReaderWriter.cpp
@@ -2,20 +2,21 @@
 using std::cerr;
 using std::endl;
 using std::ofstream;
+using namespace std;
 
-FileReaderWriter::FileReaderWriter(string fileName) : file(file) {ofstream MyWriteFile("filename.txt");};
+FileReaderWriter::FileReaderWriter(string m_file) : m_file(m_file) {ofstream MyWriteFile(m_file);};
 
 FileReaderWriter::~FileReaderWriter() {};
 
 void FileReaderWriter::write(string line) {
     ofstream file;
-    file.open(file,ios::out);
+    file.open(m_file ,ios::out);
     file << line;
     file.close();
 }
 void FileReaderWriter::open() {
     ofstream file;
-    file.open(path);
+    file.open(m_file);
 }
 void FileReaderWriter::close() {
     ofstream file;
