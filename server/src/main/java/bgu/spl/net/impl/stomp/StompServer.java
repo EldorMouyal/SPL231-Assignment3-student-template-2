672a9d2deb2e65e418deacabc39b2b46a1d9a6b7
omereliy <omereliy@post.bgu.ac.il>
2023-01-13 20:00:04 +0200

    changes for server

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -7,19 +7,26 @@ import bgu.spl.net.srv.Server;
 public class StompServer {
 
     public static void main(String[] args) {
+        //args= new String[]{"7777","tpc"};
+        if(args.length>=2)
+        {
         if(args[1].equals("reactor"))
             Server.reactor(
                     Runtime.getRuntime().availableProcessors(),
-                    7777, //port
+                    Integer.parseInt(args[0]), //port
                     () -> new StompMessagingProtocolImpl(), //protocol factory
                     () -> new StompMessageEncoderDecoder() //message encoder decoder factory
             ).serve();
         else
        if(args[1].equals("tpc"));
        Server.threadPerClient(
-        7777, //port
+        Integer.parseInt(args[0]), //port
         () -> new StompMessagingProtocolImpl(), //protocol factory
         () -> new StompMessageEncoderDecoder() //message encoder decoder factory
         ).serve();
     }
+    else{
+        throw new RuntimeException("Please enter a port and a protocol type (tpc/reactor)");
+    }
+}
 }
